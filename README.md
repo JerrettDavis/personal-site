3088995451cf86864311e5f9f02084b668e5fc01
Jerrett Davis <jd.lewis@omahealth.onmicrosoft.com>
2023-08-15 22:50:05 -0500

    Added vercel and google analytics to the site.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # My Personal Site (JerrettDavis.com)
 
+![Vercel](https://vercelbadge.vercel.app/api/jerrettdavis/personal-site)
+[![GitHub deployments](https://img.shields.io/github/deployments/jerrettdavis/personal-site/production?label=production)](https://github.com/jerrettdavis/personal-site/deployments/activity_log?environment=Production)
+
+
 This is the current iteration of my personal website. This is the first in a series of experiments to
 replace my legacy WordPress site with something a bit more... modern. As such, I elected to build this
 project using entirely new (to me) technologies, including [Next.js](https://nextjs.org), 
 [React](https://reactjs.org), and [Static Site Generators](https://en.wikipedia.org/wiki/Web_template_system#Static_site_generators).
-
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/global.css'
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
 import { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-0MMCFXZ34X" />
+            <Script id="google-analytics">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+         
+                  gtag('config', 'G-0MMCFXZ34X');
+                `}
+            </Script>
+        </>
+    )
 }
